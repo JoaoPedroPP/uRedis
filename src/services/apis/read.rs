cc39fef8bad2958b6879c44d4ea363c0e46cf862
JoaoPedroPP <jp052@hotmail.com>
2022-09-23 19:13:47 -0300

    :construction: get working fine

--- src/services/apis/read.rs
+++ src/services/apis/read.rs
@@ -15,8 +15,9 @@ pub async fn read_record(pool: web::Data<Pool>, req: web::Json<Body>) -> Result<
     let body: Body = req.clone();
     log::info!("-----------> New Read Request Incoming");
     let result: String = get(&pool, &body.key).await.map_err(|pool_error| error::ErrorNotAcceptable(format!("{}", pool_error)))?;
-    let x: serde_json::Value = serde_json::from_str(&result).unwrap();
-    let resp = GETResponse::resp_200(x);
-    // let resp = GETResponse::no_payload();
+    let resp: GETResponse = match serde_json::from_str(&result) {
+        Ok(payload) => GETResponse::resp_200(payload),
+        Err(_) => GETResponse::no_payload()
+    };
     return Ok(HttpResponse::Ok().json(resp));
 }
--- src/services/db/read.rs
+++ src/services/db/read.rs
@@ -7,15 +7,7 @@ use deadpool_redis::{redis::cmd, Connection, Pool, PoolError};
 pub async fn get(pool: &Pool, key: &str) -> Result<String, PoolError> {
     println!("GET redis");
     let mut conn: Connection = pool.get().await?;
-    // let pong: String = cmd("PING").query_async(&mut conn).await?;
-    let data: String = cmd("GET").arg(&[key]).query_async(&mut conn).await?;
-    println!("{:?}", data);
+    let data: String = cmd("GET").arg(&[key]).query_async(&mut conn).await.unwrap_or("Find nothing with this key.".to_string());
+    // println!("{:?}", data);
     Ok(data)
-    // let client = redis::Client::open(redis_url).unwrap();
-    // let mut con = client.get_connection().unwrap();
-    // let data: String = serde_json::to_string(&payload).unwrap();
-
-    // let cache = con.set::<&str, String, String>(key, data);
-    // println!("{:?}", cache);
-    // Ok(())
 }
