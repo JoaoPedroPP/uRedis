ad58616c032369a6ec2f2f8d43bb82fe054a9446
JoaoPedroPP <jp052@hotmail.com>
2022-09-23 23:08:38 -0300

    :art:

--- src/services/db/read.rs
+++ src/services/db/read.rs
@@ -1,13 +1,9 @@
-// extern crate redis;
+use log;
 use deadpool_redis::{redis::cmd, Connection, Pool, PoolError};
-// use actix_web::{
-//     web
-// };
 
 pub async fn get(pool: &Pool, key: &str) -> Result<String, PoolError> {
-    println!("GET redis");
+    log:info!("Query on Redis");
     let mut conn: Connection = pool.get().await?;
     let data: String = cmd("GET").arg(&[key]).query_async(&mut conn).await.unwrap_or("Find nothing with this key.".to_string());
-    // println!("{:?}", data);
     Ok(data)
 }
