d29b744a492eefbad2b7468811e66dbcb30f6222
JoaoPedroPP <jp052@hotmail.com>
2022-09-25 16:51:28 -0300

    :construction:

--- src/services/apis/read.rs
+++ src/services/apis/read.rs
@@ -14,10 +14,26 @@ use crate::services::db::get;
 pub async fn read_record(pool: web::Data<Pool>, req: web::Json<Body>) -> Result<HttpResponse, Error> {
     let body: Body = req.clone();
     log::info!("Read Request Incoming");
-    let result: String = get(&pool, &body.key).await.map_err(|pool_error| error::ErrorNotAcceptable(format!("{}", pool_error)))?;
-    let resp: GETResponse = match serde_json::from_str(&result) {
-        Ok(payload) => GETResponse::resp_200(payload),
-        Err(_) => GETResponse::no_payload(result)
+    // let result: String = get(&pool, &body.key).await.map_err(|pool_error| error::ErrorNotAcceptable(format!("{}", pool_error)))?;
+    let resp = match get(&pool, &body.key).await/*.map_err(|pool_error| error::ErrorNotAcceptable(format!("{}", pool_error)))*/ {
+        Ok(result) => {
+            // redis nao encontrou a chave correta
+            if result == None {
+                GETResponse::no_payload("No such key".to_string())
+            }
+            else {
+                // redis encontrou a chave
+                let x: String = result.unwrap();
+                match serde_json::from_str(&x) {
+                    Ok(payload) => GETResponse::resp_200(payload), // Caso seja um objeto JSON
+                    Err(_) => GETResponse::no_payload(x) // Caso seja uma String
+                }
+            }
+        },
+        Err(error) => {
+            log::warn!("Error");
+            GETResponse::no_payload("No such key".to_string())
+        }
     };
     return Ok(HttpResponse::Ok().json(resp));
 }
